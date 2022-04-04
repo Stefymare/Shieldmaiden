c81be2fd1911cc94a8cf748e19f436f1e9d4e209
culfri09 <95087997+culfri09@users.noreply.github.com>
2022-04-04 13:57:02 +0200

    fix weird shieldmaiden, fix axe and hand, fix enemies damage behavior, fix Marta's Unity

--- Shieldmaiden/Assets/Scripts/enemyBehavior.cs
+++ Shieldmaiden/Assets/Scripts/enemyBehavior.cs
@@ -11,14 +11,20 @@ public class enemyBehavior : MonoBehaviour
     public Transform playerposition;
     [SerializeField] public int enemySpeed;
 
+    public bool TakeDamage = false;
+
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "Weapon")
         {
+            
           //int weapondmg = col.gameObject.GetComponent<int>();
             e_animator.SetTrigger("Damaged");
             //Destroy(this.gameObject);
-            health = health - weapondmg;
+            if(TakeDamage == false) {
+                health = health - weapondmg;
+            }   
+            TakeDamage = true;
         }
     }
 
@@ -33,10 +39,13 @@ public class enemyBehavior : MonoBehaviour
       void Update()
         {
 
+
         playerposition = player.GetComponent<Transform>();
-        transform.position = Vector3.MoveTowards(transform.position, playerposition.position, enemySpeed * Time.deltaTime);
+      //  transform.position = Vector3.MoveTowards(transform.position, playerposition.position, enemySpeed * Time.deltaTime);
         
         e_animator.SetInteger("State", 1);
+
+        TakeDamage = false;
 
 
          if ((transform.position - playerposition.position).magnitude <= 0.8f)
