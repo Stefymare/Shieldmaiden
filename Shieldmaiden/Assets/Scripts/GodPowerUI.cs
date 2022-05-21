7094a5fee681c8d496b90d7772436746b161323a
culfri09 <95087997+culfri09@users.noreply.github.com>
2022-05-21 16:39:51 +0200

    UI fix

---
+++ Shieldmaiden/Assets/Scripts/GodPowerUI.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GodPowerUI : MonoBehaviour
+{
+
+    public string thorPower;
+
+    float cooldown;
+    public float coolTime;
+    public Image cdImg;
+    bool cool;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            print("key pressed");
+            Power();
+        }
+    }
+
+    public void Power()
+    {
+        if (!cool)
+        {
+            cool = true;
+            StartCoroutine("CoolDown");
+        }
+
+    }
+    IEnumerator CoolDown()
+    {
+        cooldown = 0;
+
+        while (true)
+        {
+            cooldown += Time.deltaTime;
+            cdImg.fillAmount = cooldown / coolTime;
+
+            if (cooldown >= coolTime)
+            {
+                cool = false;
+                StopCoroutine("CoolDown");
+            }
+
+            yield return null;
+        }
+    }
+}
